e8e5e024f7716d340c15071ae2182996ce009094
tanahashi <l4048280@gmail.com>
2024-05-20 16:45:29 +0900

    java-mid2 3rd commit

---
+++ src/collection/set/JavaHashCodeMain.java
@@ -0,0 +1,15 @@
+package collection.set;
+
+public class JavaHashCodeMain {
+
+    public static void main(String[] args) {
+
+        Object obj1 = new Object();
+        Object obj2 = new Object();
+        System.out.println("obj1.hashCode() = " + obj1.hashCode());
+        System.out.println("obj2.hashCode() = " + obj2.hashCode());
+
+
+
+    }
+}
